ba840379357b5d83030d063abd74b446b7910297
Bram.Cautaerts <bram.cautaerts@ing.com>
2025-05-06 11:38:08 +0200

    smaller templates

--- src/constellations_list.template.hpp
+++ src/constellations_list.template.hpp
@@ -26,12 +26,10 @@ inline const std::string res_template = R"(
 │%2│      ^[1;37m%11^[0m
 │%3│
 │%4│      ^[1;37mQuadrant^[0m: %12
-│%5│      ^[1;37mRight ascension^[0m: %13
-│%6│      ^[1;37mDeclination^[0m: %14
-│%7│      ^[1;37mArea^[0m: %15
-│%8│      ^[1;37mMain stars^[0m: %16
-│%9│
-│%10│
+│%5│      ^[1;37mArea^[0m: %15
+│%6│      ^[1;37mMain stars^[0m: %16
+│%7│
+│%8│
 └──────────────────────┘
 )";
 
@@ -46,17 +44,13 @@ inline std::unordered_map<std::string, std::string> res_constellations_json_list
                 "line2": { },
                 "line3": { "15": "✦" },
                 "line4": { },
-                "line5": { "8":"✦"},
-                "line6": { "19": "✦"},
+                "line5": { },
+                "line6": { "4": "✦" },
                 "line7": { },
-                "line8": { "4": "✦" },
-                "line9": { },
-                "line10": { }
+                "line8": { }
             },
         "name": "Antlia",
         "quadrant": "SQ2",
-        "right ascension": "09h 27m 05.1837s to 11h 05m 55.0471s",
-        "declination": "-24.5° to -40.4°",
         "area": "239 sq.deg. {62nd}",
         "main stars": "3"
     }
@@ -72,17 +66,13 @@ inline std::unordered_map<std::string, std::string> res_constellations_json_list
                 "line2": { },
                 "line3": { },
                 "line4": { "4":"✦"},
-                "line5": { "10":"✦"},
-                "line6": { "7":"✦"},
-                "line7": { "19":"✦"},
-                "line8": { },
-                "line9": { },
-                "line10": { }
+                "line5": { "19":"✦"},
+                "line6": { },
+                "line7": { },
+                "line8": { }
             },
         "name": "Apus",
         "quadrant": "SQ3",
-        "right ascension":"13h 51m 07.5441s to 18h 27m 27.8395s",
-        "declination": "−67.4800797° to −83.1200714°",
         "area": "206 sq.deg. {67th}",
         "main stars": "4"
     }
@@ -98,17 +88,13 @@ inline std::unordered_map<std::string, std::string> res_constellations_json_list
                 "line2": { "10": "✦",  "11": "✦" },
                 "line3": { "12": "✦",  "15": "✦" },
                 "line4": { "4": "✦" },
-                "line5": { "6": "✦",  "17": "✦" },
-                "line6": { "12": "✦" },
-                "line7": { "7": "✦" },
-                "line8": { "5": "✦",  "12": "✦" },
-                "line9": { "4": "✦",  "19": "✦" },
-                "line10": { }
+                "line5": { "7": "✦" },
+                "line6": { "5": "✦",  "12": "✦" },
+                "line7": { "4": "✦",  "19": "✦" },
+                "line8": { }
             },
         "name": "Aquarius",
         "quadrant": "SQ4",
-        "right ascension": "20h38m to 23h56m",
-        "declination": "+3.325° to -24.904°",
         "area": "980 sq.deg. {10th}",
         "main stars": "10, 22"
         }
@@ -126,16 +112,12 @@ inline std::unordered_map<std::string, std::string> res_constellations_json_list
                     "line3": { "16":"✦"},
                     "line4": { },
                     "line5": { },
-                    "line6": { "16":"✦","9":"✦"},
-                    "line7": { },
-                    "line8": { },
-                    "line9": { "19":"✦"},
-                    "line10": { "6":"✦"}
+                    "line6": { },
+                    "line7": { "19":"✦"},
+                    "line8": { "6":"✦"}
                 },
             "name": "Ara",
             "quadrant": "SQ3",
-            "right ascension":"16h 34m 16.9497s to –18h 10m 41.3407s",
-            "declination": "−45.4859734° to −67.6905823°",
             "area": "237 sq.deg. {63rd}",
             "main stars": "8"
         }
@@ -151,17 +133,13 @@ inline std::unordered_map<std::string, std::string> res_constellations_json_list
                     "line2": { },
                     "line3": { "5": "✦" },
                     "line4": { },
-                    "line5": { "15": "✦" },
-                    "line6": { },
-                    "line7": { "17": "✦" },
-                    "line8": { "16": "✦" },
-                    "line9": { },
-                    "line10": { }
+                    "line5": { "17": "✦" },
+                    "line6": { "16": "✦" },
+                    "line7": { },
+                    "line8": { }
                 },
             "name": "Aries",
             "quadrant": "NQ1",
-            "right ascension": "01h 46m 37.3761s to –03h 29m 42.4003s",
-            "declination": "+31.2213154° to –10.3632069°",
             "area": "441 sq.deg. {39th}",
             "main stars": "4, 9"
         }
@@ -177,17 +155,13 @@ inline std::unordered_map<std::string, std::string> res_constellations_json_list
                     "line2": { "7":"✦"},
                     "line3": { "12":"✦"},
                     "line4": { "4":"✦"},
-                    "line5": { "12":"✦"},
-                    "line6": { "8":"✦"},
-                    "line7": { },
-                    "line8": { "13":"✦"},
-                    "line9": { "8":"✦", "17":"✦"},
-                    "line10": { "7":"✦", "18":"✦"}
+                    "line5": { },
+                    "line6": { "13":"✦"},
+                    "line7": { "8":"✦", "17":"✦"},
+                    "line8": { "7":"✦", "18":"✦"}
                 },
             "name": "Boötes",
             "quadrant": "NQ3",
-            "right ascension":"13h 36.1m to 15h 49.3m",
-            "declination": "+7.36° to +55.1°",
             "area": "907 sq.deg. {13th}",
             "main stars": "7, 15"
         }
@@ -203,17 +177,13 @@ inline std::unordered_map<std::string, std::string> res_constellations_json_list
                     "line2": { "7": "✦" },
                     "line3": { },
                     "line4": { "10": "✦" },
-                    "line5": { "11": "✦" },
-                    "line6": { },
-                    "line7": { "17": "✦" },
-                    "line8": { "8": "✦" },
-                    "line9": { },
-                    "line10": { }
+                    "line5": { "17": "✦" },
+                    "line6": { "8": "✦" },
+                    "line7": { },
+                    "line8": { }
                 },
             "name": "Cancer",
             "quadrant": "NQ1",
-            "right ascension": "07h 55m 19.7973s to –09h 22m 35.0364s",
-            "declination": "+33.1415138° to –6.4700689°",
             "area": "506 sq.deg. {31st}",
             "main stars": "5"
         }
@@ -229,17 +199,13 @@ inline std::unordered_map<std::string, std::string> res_constellations_json_list
                     "line2": { },
                     "line3": { "3": "✦", "19": "✦" },
                     "line4": { "5": "✦", "8": "✦", "12": "✦", "18": "✦" },
-                    "line5": { "17": "✦" },
-                    "line6": { "5": "✦" },
-                    "line7": { "7": "✦", "14": "✦" },
-                    "line8": { "12": "✦" },
-                    "line9": { },
-                    "line10": { }
+                    "line5": { "7": "✦", "14": "✦" },
+                    "line6": { "12": "✦" },
+                    "line7": { },
+                    "line8": { }
                 },
             "name": "Capricorn",
             "quadrant": "SQ4",
-            "right ascension": "20h 06m 46.4871s to 21h 59m 04.8693s",
-            "declination": "−8.4043999° to −27.6914144°",
             "area": "414 sq.deg. {40th}",
             "main stars": "9, 13, 23"
         }
@@ -255,17 +221,13 @@ inline std::unordered_map<std::string, std::string> res_constellations_json_list
                     "line2": { },
                     "line3": { "4": "✦"},
                     "line4": { },
-                    "line5": { "7": "✦", "12": "✦" },
-                    "line6": { "20": "✦" },
+                    "line5": { },
+                    "line6": { "16": "✦" },
                     "line7": { },
-                    "line8": { "16": "✦" },
-                    "line9": { },
-                    "line10": { }
+                    "line8": { }
                 },
             "name": "Cassiopeia",
             "quadrant": "NQ1",
-            "right ascension": "22h 57m 04.5897s to –03h 41m 14.0997s",
-            "declination": "+77.6923447° to +48.6632690°",
             "area": "598 sq.deg. {25th}",
             "main stars": "5"
         }
@@ -281,17 +243,13 @@ inline std::unordered_map<std::string, std::string> res_constellations_json_list
                     "line2": { },
                     "line3": { "15": "✦"},
                     "line4": { },
-                    "line5": { "3": "✦", "20": "✦" },
-                    "line6": { },
-                    "line7": { "5": "✦", "18": "✦"},
-                    "line8": { "10": "✦", "13": "✦" },
-                    "line9": { },
-                    "line10": { }
+                    "line5": { "5": "✦", "18": "✦"},
+                    "line6": { "10": "✦", "13": "✦" },
+                    "line7": { },
+                    "line8": { }
                 },
             "name": "Corona Borealis",
             "quadrant": "NQ1",
-            "right ascension": "15h 16m 03.8205s to 16h 25m 07.1526s",
-            "declination": "+39.7117195° to +25.5380573°",
             "area": "179 sq.deg. {73rd}",
             "main stars": "8"
         }
@@ -307,17 +265,13 @@ inline std::unordered_map<std::string, std::string> res_constellations_json_list
                     "line2": { "10":"✦"},
                     "line3": { },
                     "line4": { "16":"✦"},
-                    "line5": { "5":"✦"},
+                    "line5": { },
                     "line6": { },
-                    "line7": { },
-                    "line8": { },
-                    "line9": { "13":"✦"},
-                    "line10": { }
+                    "line7": { "13":"✦"},
+                    "line8": { }
                 },
             "name": "Crux",
             "quadrant": "SQ3",
-            "right ascension":"12.5h",
-            "declination": "−60°",
             "area": "68 sq.deg. {88th}",
             "main stars": "4"
         }
@@ -333,17 +287,13 @@ inline std::unordered_map<std::string, std::string> res_constellations_json_list
                     "line2": { "18": "✦" },
                     "line3": { "9": "✦" },
                     "line4": { "15": "✦" },
-                    "line5": { "11": "✦" },
-                    "line6": { },
-                    "line7": { "7": "✦", "15": "✦"},
-                    "line8": { "4": "✦" },
-                    "line9": { "18": "✦" },
-                    "line10": { }
+                    "line5": { "7": "✦", "15": "✦"},
+                    "line6": { "4": "✦" },
+                    "line7": { "18": "✦" },
+                    "line8": { }
                 },
             "name": "Cygnus",
             "quadrant": "NQ4",
-            "right ascension": "20.62h",
-            "declination": "+42.03°",
             "area": "804 sq.deg. {16th}",
             "main stars": "9"
         }
@@ -360,17 +310,13 @@ inline std::unordered_map<std::string, std::string> res_constellations_json_list
                     "line2": { "7": "✦", "14": "✦" },
                     "line3": { "4": "✦", "11": "✦", "15": "✦", "19": "✦" },
                     "line4": { },
-                    "line5": { "6": "✦" },
-                    "line6": { "4": "✦", "8": "✦", "15": "✦" },
-                    "line7": { },
-                    "line8": { },
-                    "line9": { "4": "✦", "8": "✦", "14": "✦", "17": "✦" },
-                    "line10": { "18": "✦" }
+                    "line5": { },
+                    "line6": { },
+                    "line7": { "4": "✦", "8": "✦", "14": "✦", "17": "✦" },
+                    "line8": { "18": "✦" }
                 },
             "name": "Gemini",
             "quadrant": "NQ2",
-            "right ascension": "7h",
-            "declination": "+20°",
             "area": "514 sq.deg. {30th}",
             "main stars": "8,17"
         }
---
+++ src/starfetch.cpp
@@ -0,0 +1,137 @@
+#include <random>
+#include <algorithm>
+#include <iostream>
+
+#include "_gen_constellations_list.hpp"
+
+#include "include/json.hpp"
+using json = nlohmann::json;
+
+void PrintConst(const std::string &constellation_info);   // formats the template file with the requested data and prints out the constellation info
+std::string RandomConst();                   // select a random constellation from the available ones
+void PrintList();                            // prints out the list of the available constellations
+void Error(const std::string &err, int code);// shows an error message
+void Help();                                 // prints out the help message
+
+int main(int argc, char *argv[]) {
+    std::string constellation_info;
+    if (argc == 1) {             // if there's no additional arguments
+        constellation_info = RandomConst();  // selects a random constellation
+    } else {
+        switch (argv[1][1])// gets the time of the argument (the 'n' in "-n")
+        {
+            case 'n':
+                if (argc < 3) {
+                    Error(" ", 0);
+                }
+                else if(!res_constellations_json_list.count(argv[2])){
+                    Error(" ", 2);
+                }
+                else{
+                    constellation_info = res_constellations_json_list[argv[2]];
+                }
+                break;
+            case 'h':
+                Help();
+                break;
+            case 'r':
+                constellation_info = RandomConst();// with the '-r' option, it selects a random constellation
+                break;
+            case 'l':
+                PrintList();
+            default:
+                Error(argv[1], 1);// if the reqeusted option isn't recognized, an error occours
+        }
+    }
+
+    PrintConst(constellation_info);// prints the constellation
+    return 0;
+}
+
+void PrintConst(const std::string &constellation_info) {
+    json json_data = json::parse(constellation_info);
+    std::replace(res_template.begin(), res_template.end(), '^', '\033');// replace '^' with the '\e' to print bold/colored text
+
+    // fills the template with dats
+    res_template.replace(res_template.find("%0"), std::string("%0").size(),
+                         json_data["title"].get<std::string>());
+    res_template.replace(res_template.find("%11"), std::string("%11").size(),
+                         json_data["name"].get<std::string>());
+    res_template.replace(res_template.find("%12"), std::string("%12").size(),
+                         json_data["quadrant"].get<std::string>());
+    res_template.replace(res_template.find("%15"), std::string("%15").size(),
+                         json_data["area"].get<std::string>());
+    res_template.replace(res_template.find("%16"), std::string("%16").size(),
+                         json_data["main stars"].get<std::string>());
+
+    // renders the constellation's graph from the coordinates specified in the JSON data
+    std::string l_tmp;
+    for (int i = 1; i <= 8; i++)// for each of the lines (10)
+    {
+        l_tmp = "";
+        for (int k = 1; k <= 22; k++) {// for each of the columns of the graph (22)
+            // if the JSON file specifies a star at position k
+            if (json_data["graph"]["line" + std::to_string(i)].find(std::to_string(k)) !=
+                json_data["graph"]["line" + std::to_string(i)].end()) {
+                l_tmp += json_data["graph"]["line" + std::to_string(i)][std::to_string(k)]
+                             .get<std::string>();// put the star (which is stored into the JSON fine, might change this in the future)
+            } else {
+                l_tmp += " ";// put a space
+            }
+        }
+
+        // insert the line into the template
+        res_template.replace(res_template.find("%" + std::to_string(i)),
+                             std::string("%" + std::to_string(i)).size(), l_tmp);
+    }
+    std::cout << res_template << std::endl;// prints the output
+}
+
+std::string RandomConst() {
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_int_distribution<> dist(0, res_constellations_json_list.size() - 1);
+
+    auto iter = res_constellations_json_list.begin();
+    std::advance(iter, dist(mt));
+
+    return iter->second;
+}
+
+void PrintList() {
+    size_t pos;
+    std::string star_name;
+
+    std::cout << "✦ \e[1;37mavailable constellations\e[0m: " << std::endl;
+    // prints every constellation name from the list
+    for (const auto &[key, _] : res_constellations_json_list){
+        std::cout << key << std::endl;
+    }
+    exit(0);
+}
+
+void Error(const std::string &err, int code) {
+    switch (code)// each error has a specific code
+    {
+        case 0:// 0 for the missing input
+            std::cout << "Error: you must input a constellation name after -n."
+                      << std::endl
+                      << std::endl;
+            break;
+        case 1:// 1 for the invalid argument
+            std::cout << "Error: '" << err << "' isn't a valid argument." << std::endl
+                      << std::endl;
+            break;
+        case 2:// 2 for the invalid constellation name
+            std::cout << "Error: the constellation you asked for isn't recognized. "
+                      << std::endl
+                      << std::endl;
+    }
+
+    Help();// after any error occours, the help message is shown
+}
+
+void Help() {
+    std::cout << res_help_message;
+    exit(0);
+}
